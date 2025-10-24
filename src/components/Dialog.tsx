9096c71ea9c2e407674113412e4d5f757e4f0bf5
Miguel Sosa <57022857+LordSinSentido@users.noreply.github.com>
2025-10-24 21:15:11 +0000

    Fix body scroll when modal is opened

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,16 +1,31 @@
+import { useEffect } from 'react'
+
 export default function Dialog({
   isOpen,
   onClose,
-  children,
+  children
 }: {
   isOpen: boolean
   onClose: () => void
   title: String
   children: React.ReactElement
 }) {
+  useEffect(() => {
+    if (isOpen) {
+      const prev = document.body.style.overflow
+      document.body.style.overflow = 'hidden'
+      return () => {
+        document.body.style.overflow = prev
+      }
+    }
+  }, [isOpen])
+
   if (!isOpen) return null
   return (
-    <div className='dialog overflow-x-scroll' onClick={() => onClose()}>
+    <div
+      className='bg-light-surface-dim/75 dark:bg-dark-surface-dim/75 backdrop-blur-xs fixed inset-0 md:p-10 p-2 z-50 flex justify-center items-start overflow-scroll'
+      onClick={() => onClose()}
+    >
       <div className='max-w-3xl' onClick={(e) => e.stopPropagation()}>
         {children}
       </div>
