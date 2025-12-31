003cdadd6b050d7f0f68e994c9cb3c048e21d223
Miguel Sosa <57022857+LordSinSentido@users.noreply.github.com>
2025-12-31 13:03:47 -0600

    Create App

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+import Header from '@/sections/Header'
+import Skills from '@/sections/Skills'
+import Projects from '@/sections/Projects'
+import Jobs from '@/sections/Jobs'
+import Contact from '@/sections/Contact'
+import Navbar from '@/sections/Navbar'
+
+function App() {
+  return (
+    <main className='flex items-center justify-center'>
+      <div className='flex flex-col items-center min-h-screen w-full max-w-374 pb-4 md:pb-8'>
+        <Navbar />
+        <Header />
+        <Skills />
+        <Projects />
+        <Jobs />
+        <Contact />
+      </div>
+    </main>
+  )
+}
+
+export default App
