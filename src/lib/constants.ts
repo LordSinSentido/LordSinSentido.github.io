b24b3a671af2630bbbc0cce0c6d49c2132ba595a
Miguel Sosa <57022857+LordSinSentido@users.noreply.github.com>
2025-12-31 13:02:33 -0600

    Create constants file

---
+++ src/lib/constants.ts
@@ -0,0 +1,7 @@
+import type { Easing } from 'motion'
+
+export const animationDuration: number = 0.2
+export const animationEase: Easing = 'easeOut'
+export type Status = 'loading' | 'done' | 'failed'
+export type Modality = 'full-time' | 'part-time' | 'freelance' | 'contract'
+export type Type = 'hybrid' | 'on-site' | 'remote'
