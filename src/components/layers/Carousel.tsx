63c4cadbff7bbd9cfcbc4bc0597b86b9b251fc53
Miguel Sosa <57022857+LordSinSentido@users.noreply.github.com>
2026-01-02 10:58:24 -0600

    Update classnames

--- src/components/layers/Carousel.tsx
+++ src/components/layers/Carousel.tsx
@@ -6,9 +6,9 @@ interface Carousel {
 
 export default function Carousel({ children }: Carousel) {
   return (
-    <div className='flex gap-2 overflow-x-auto overflow-y-hidden scroll-smooth snap-x snap-mandatory scrollbar-none scrollbar-hide pb-1'>
+    <div className='flex gap-2 overflow-x-auto  scroll-smooth snap-x snap-mandatory scrollbar-none scrollbar-hide pb-1'>
       {Children.map(children, (child) => (
-        <div className='flex-shrink-0 w-[84dvw] max-w-120 snap-start h-full md:scroll-ml-8 scroll-ml-4'>
+        <div className='shrink-0 w-[84dvw] max-w-120 snap-start h-full md:scroll-ml-8 scroll-ml-4'>
           {child}
         </div>
       ))}
