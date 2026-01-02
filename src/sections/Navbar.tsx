baaf50407a6177b076012f3a66823074ce087dfe
Miguel Sosa <57022857+LordSinSentido@users.noreply.github.com>
2026-01-02 10:58:05 -0600

    Implement overlay

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -6,10 +6,11 @@ import Link from '@/ui/Link'
 import Logo from '@/ui/Logo'
 import Stack from '@/layers/Stack'
 import { animationDuration, animationEase } from '@/lib/constants'
+import Overlay from '@/components/layers/Overlay'
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState<boolean>(false)
-  const handleDrawer = () => setIsOpen(!isOpen)
+  const handleOverlay = () => setIsOpen(!isOpen)
 
   const Sections = () => {
     const sections = [
@@ -21,7 +22,7 @@ export default function Navbar() {
     return (
       <>
         {sections.map((section) => (
-          <Link href={section.ref} onClick={handleDrawer} target='_self'>
+          <Link href={section.ref} onClick={handleOverlay} target='_self'>
             {section.name}
           </Link>
         ))}
@@ -35,7 +36,7 @@ export default function Navbar() {
         <Logo className='fill-primary size-6' />
       </Link>
 
-      <nav className='fixed top-2 right-2 z-50'>
+      <nav className='fixed top-2 right-2 z-30'>
         <Stack
           direction='horizontal'
           className='sm:visible sm:flex invisible hidden'
@@ -43,11 +44,11 @@ export default function Navbar() {
           <Sections />
         </Stack>
 
-        <Stack
-          direction='vertical'
-          className='sm:invisible visible items-end z-50'
-        >
-          <Button className='z-50' onClick={() => setIsOpen(!isOpen)}>
+        <Stack direction='vertical' className='sm:invisible visible items-end'>
+          <Button
+            className={`${isOpen ? 'z-30' : ''}`}
+            onClick={() => setIsOpen(!isOpen)}
+          >
             <AnimatePresence mode='wait' initial={false}>
               {isOpen ? (
                 <motion.div
@@ -74,25 +75,7 @@ export default function Navbar() {
           </Button>
 
           <AnimatePresence>
-            {isOpen && (
-              <motion.div
-                key='Overlay'
-                className={`fixed inset-0 bg-black/40`}
-                initial='closed'
-                animate='opened'
-                exit='closed'
-                variants={{
-                  opened: { opacity: 1, pointerEvents: 'auto' },
-                  closed: { opacity: 0, pointerEvents: 'none' }
-                }}
-                transition={{
-                  duration: animationDuration,
-                  ease: animationEase
-                }}
-                onClick={handleDrawer}
-                id='overlay'
-              />
-            )}
+            {isOpen && <Overlay callback={handleOverlay} />}
           </AnimatePresence>
 
           <AnimatePresence>
@@ -110,7 +93,7 @@ export default function Navbar() {
                   duration: animationDuration,
                   ease: animationEase
                 }}
-                className='overflow-hidden  z-50'
+                className='overflow-hidden z-30'
                 id='collapsible-content'
               >
                 <Stack className='items-end'>
