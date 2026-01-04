919dd4f7cf64e3aaf80192de397e178dc0ec9ebf
Miguel Sosa <57022857+LordSinSentido@users.noreply.github.com>
2026-01-04 15:44:20 -0600

    Fix flex alignment

--- src/components/typography/Quote.tsx
+++ src/components/typography/Quote.tsx
@@ -6,7 +6,7 @@ interface QuoteProps {
 
 export default function Quote({ children }: QuoteProps) {
   return (
-    <Section className='justify-center'>
+    <Section className='items-center'>
       <q className='font-cursive text-2xl text-center font-semibold max-w-4xl leading-10'>
         {children}
       </q>
