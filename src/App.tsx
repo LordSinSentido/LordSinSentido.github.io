29b6c3103b2da96b82bdee7dfbe02ae0aab794a8
Miguel Sosa <57022857+LordSinSentido@users.noreply.github.com>
2026-01-04 15:29:20 -0600

    Implement routing and context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,74 @@
 import './App.css'
-import Header from '@/sections/Header'
-import Skills from '@/sections/Skills'
-import Projects from '@/sections/Projects'
-import Jobs from '@/sections/Jobs'
-import Contact from '@/sections/Contact'
-import Navbar from '@/sections/Navbar'
+import { AnimatePresence, motion } from 'motion/react'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { animationDuration, animationEase } from '@/lib/constants'
+import Home from '@/pages/Home'
+import Navigation from '@/ui/Navigation'
+import Jobs from '@/pages/Jobs'
+import Projects from '@/pages/Projects'
+import Contact from '@/pages/Contact'
+
+const PageTransition = ({ children }: { children: React.ReactNode }) => (
+  <motion.div
+    variants={{
+      initial: { opacity: 0, y: '1rem' },
+      animate: { opacity: 1, y: 0 }
+    }}
+    initial='initial'
+    animate='animate'
+    exit='initial'
+    transition={{ duration: animationDuration, ease: animationEase }}
+    className='rounded-xl overflow-hidden'
+  >
+    {children}
+  </motion.div>
+)
 
 function App() {
+  const location = useLocation()
+
   return (
-    <main className='flex items-center justify-center pb-40'>
-      <div className='flex flex-col items-center min-h-screen w-full max-w-374 pb-4 md:pb-8'>
-        <Navbar />
-        <Header />
-        <Skills />
-        <Projects />
-        <Jobs />
-        <Contact />
-      </div>
-    </main>
+    <div className='flex flex-col sm:flex-row p-4 gap-4 h-dvh'>
+      <Navigation />
+      <main className='flex-1 relative rounded-xl overflow-y-scroll h-full overscroll-none max-w-480 pb-10'>
+        <AnimatePresence mode='wait'>
+          <Routes location={location} key={location.pathname}>
+            <Route
+              path='/'
+              element={
+                <PageTransition>
+                  <Home />
+                </PageTransition>
+              }
+            />
+            <Route
+              path='/experience'
+              element={
+                <PageTransition>
+                  <Jobs />
+                </PageTransition>
+              }
+            />
+            <Route
+              path='/projects'
+              element={
+                <PageTransition>
+                  <Projects />
+                </PageTransition>
+              }
+            />
+            <Route
+              path='/contact'
+              element={
+                <PageTransition>
+                  <Contact />
+                </PageTransition>
+              }
+            />
+          </Routes>
+        </AnimatePresence>
+      </main>
+    </div>
   )
 }
 
