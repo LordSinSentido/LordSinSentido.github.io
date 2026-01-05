a012ead0c7f004bcb875691e9b50d52be639a6dc
Miguel Sosa <57022857+LordSinSentido@users.noreply.github.com>
2026-01-05 13:42:34 -0600

    Implement SEO

--- src/pages/Jobs.tsx
+++ src/pages/Jobs.tsx
@@ -9,6 +9,7 @@ import Text from '@/typography/Text'
 import Collapsable from '@/ui/Collapsable'
 import List from '@/ui/List'
 import Loading from '@/ui/Loading'
+import SEO from '@/context/SEO'
 
 export default function Jobs() {
   const { jobs, getJobs } = useFirestoreContext()
@@ -19,32 +20,38 @@ export default function Jobs() {
 
   if (jobs) {
     return (
-      <Stack>
-        <Title>Experience</Title>
-        <Section>
-          <Stack>
-            {jobs.map((job) => (
-              <Collapsable
-                key={job.id}
-                header={
-                  <Stack gap='1'>
-                    <Headline>
-                      {job.position} @ {job.organization}
-                    </Headline>
-                    <Subheadline>{job.location}</Subheadline>
-                  </Stack>
-                }
-                content={
-                  <Stack className='mt-2'>
-                    <Text>{job.description}</Text>
-                    <List items={job.goals} />
-                  </Stack>
-                }
-              />
-            ))}
-          </Stack>
-        </Section>
-      </Stack>
+      <>
+        <SEO
+          title='Jobs'
+          description='Here you can see the companies where I worked.'
+        />
+        <Stack>
+          <Title>Experience</Title>
+          <Section>
+            <Stack>
+              {jobs.map((job) => (
+                <Collapsable
+                  key={job.id}
+                  header={
+                    <Stack gap='1'>
+                      <Headline>
+                        {job.position} @ {job.organization}
+                      </Headline>
+                      <Subheadline>{job.location}</Subheadline>
+                    </Stack>
+                  }
+                  content={
+                    <Stack className='mt-2'>
+                      <Text>{job.description}</Text>
+                      <List items={job.goals} />
+                    </Stack>
+                  }
+                />
+              ))}
+            </Stack>
+          </Section>
+        </Stack>
+      </>
     )
   } else {
     return <Loading />
